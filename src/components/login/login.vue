95b1d290779f3b03317fb0e16aea5f943bf1ce21
youTT <cdyoutingting1@jd.com>
2018-03-16 14:04:14 +0800

    computed get set v-model

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="login-body">
     <div class="login-wrap">
-      <input type="text" placeholder="input name" ref="nameRef"/>
-      <input type="password" placeholder="input password" ref="passRef"/>
+      <input type="text" placeholder="input name" v-model="pageUserName" ref="nameRef"/>
+      <input type="password" placeholder="input password" v-model="pagePassword" ref="passRef"/>
       <button v-on:click = 'login'>确认提交</button>
     </div>
   </div>
@@ -10,16 +10,36 @@
 
 <script>
 import { mapState } from 'vuex'
+import * as types from '../../store/login/mutationsType'
 export default {
-  computed: mapState({
-    userName: state => state.login.userName,
-    password: state => state.login.password,
-    loginSuccess: state => state.login.loginSuccess
-  }),
+  computed: {
+    ...mapState({
+      userName: state => state.login.userName,
+      password: state => state.login.password,
+      loginSuccess: state => state.login.loginSuccess
+    }),
+    pageUserName: {
+      get: function () {
+        return this.userName
+      },
+      set: function (val) {
+        this.$store.commit(types.SET_USER_NAME, val)
+      }
+    },
+    pagePassword: {
+      get: function () {
+        return this.password
+      },
+      set: function (val) {
+        this.$store.commit(types.SET_USER_PASSWORD, val)
+      }
+    }
+
+  },
   methods: {
     login(){
-      var name = this.$refs.nameRef.value
-      var password = this.$refs.passRef.value
+      var name = this.userName
+      var password = this.password
       this.$store.dispatch('login',
         {
           userName: name,
--- src/store/login/mutations.js
+++ src/store/login/mutations.js
@@ -8,6 +8,12 @@ const mutations = {
   [types.USER_LOGIN_ERROR]( state, {...result}){
     alert('登录失败')
     delete window.token
+  },
+  [types.SET_USER_NAME]( state, userName){
+    state.userName = userName
+  },
+  [types.SET_USER_PASSWORD]( state, password){
+    state.password = password
   }
 }
 
