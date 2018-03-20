5510d07a3a1ee7c71cc3b33c18306ffbc29a731f
youTT <cdyoutingting1@jd.com>
2018-03-20 16:50:32 +0800

    in score.getters, getters use getters

--- src/store/rights/actions.js
+++ src/store/rights/actions.js
@@ -5,8 +5,11 @@ const actions = {
   getRightsImgs ({state, commit, rootState}, id) {
     api.getRightsImgs(
       id,
-      //action 的参数 有state,commit ,rootState
-      ({...result}) => { console.log(state); console.log(rootState); commit(types.GET_RIGHTS_IMGS, {...result})}
+      // action 的参数 有state,commit ,rootState
+      ({...result}) => {
+      // console.log(state)
+      // console.log(rootState)
+        commit(types.GET_RIGHTS_IMGS, {...result}) }
     )
   }
 }
--- src/store/scores/getters.js
+++ src/store/scores/getters.js
@@ -2,17 +2,16 @@ const getters = {
   selectedScoreList: state => {
     return state.scoreList.filter(score => score.checked === true)
   },
-  scoreAcc: state => {
+  scoreAcc: (state, getters) => {
     var scoreAcc = 0
-    state.scoreList.forEach(score => {
-      if (score.checked) {
-        scoreAcc += score.score
-      }
+    getters.selectedScoreList.forEach(score => {
+      scoreAcc += score.score
     })
     return scoreAcc
   },
-  //getters has three params, state,getters,rootState
-  selectScoreLength: (state, getters, rootState) => {
+  // getters has three params, state,getters,rootState
+  selectScoreLength: (state, getters, rootState, rootGetters) => {
+    // console.log(rootState, rootGetters)
     return getters.selectedScoreList.length
   }
 }
